8d0b6db837c993d13e30707b7394dbc6cac9d44a
Jagdeep009 <jagdeep.mahala27@gmail.com>
2024-11-24 15:00:49 +0530

    side menu css updated

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -19,9 +19,9 @@ function SideMenu() {
 
   return (
     <div className='SideMenu shadow d-flex flex-column justify-content-between'>
-      <div className='d-lg-block d-flex justify-content-between w-100'>
+      <div className='d-lg-block d-flex justify-content-between w-100  align-items-center'>
         <Logo />
-        <ul className='d-lg-block d-flex align-items-center'>
+        <ul className='d-lg-block d-md-flex d-none align-items-center'>
           {/* <li><NavLink to='/'><img src={Dashboard} /> Dashboard</NavLink></li> */}
           <li><NavLink to='/'><img src={Users} /> Users</NavLink></li>
           <li><NavLink to='/role-management'><img src={Roles} /> Role Management</NavLink></li>
@@ -30,6 +30,9 @@ function SideMenu() {
             <img src={Logout} /> <span>Logout</span>
           </div>
         </ul>
+        <div className="d-md-none d-flex menu-btn">
+          <i class="fa-solid fa-bars"></i>
+        </div>
       </div>
       <div className='Logout c-p fw-semibold d-lg-block d-none' onClick={logoutUser}>
         <img src={Logout} /> <span>Logout</span>
